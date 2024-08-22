3dc8732fcbf8bbce0725abac641cc23e28389b04
Jenny Liang <143564117+jennytliang@users.noreply.github.com>
2024-08-22 00:51:02 -0700

    Make script work with MSR 2022 data.

--- data/scripts/html_to_text.py
+++ data/scripts/html_to_text.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import json
 
-html_code_filepath = './data/scripts/raw_html.html'
+conference_name = "msr-2022"
+html_code_filepath = f'./data/scripts/{conference_name}_raw_html.html'
 html_code_open = open(html_code_filepath, 'r', encoding="UTF-8")
 html_code = html_code_open.read()
 
@@ -37,7 +38,7 @@ for idx, tag in enumerate(appended_tags):
     authors = [author.get_text(strip=True) for author in soup2.find_all('h5', class_='media-heading')]
 
     # Extract the allocated time
-    time_info = soup2.find('strong').get_text(strip=True).replace('atMeeting', 'at Meeting').replace('atOffsite', 'at Offsite')
+    time_info = soup2.find('strong').get_text(strip=True).replace('atMeeting', 'at Meeting').replace('atOffsite', 'at Offsite').replace("atMSR", "at MSR").replace("atRoom", "at Room")
     arr = time_info.split(" at ")
     time_info = arr[0]
     location_info = arr[1]
@@ -55,5 +56,5 @@ for idx, tag in enumerate(appended_tags):
 
     json_array.append(json_element)
 
-output_file = open("msr-2023.json", 'w')
+output_file = open(f"./data/scripts/{conference_name}.json", 'w')
 json.dump(json_array, output_file, indent=4)
